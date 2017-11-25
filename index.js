9873fecca658d155150904641ad3222e0f8ee443
youzhiye <zhangyouzhiye@gmail.com>
2017-11-25 19:00:46 +0800

    规范js的语法

--- index.js
+++ index.js
@@ -1,45 +1,45 @@
 //导入必要的模块
-const express = require('express')
-const ejs = require('ejs')
-const bodyParser = require("body-parser")
-const fs = require('fs')
-const os = require('os')
+const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require("body-parser");
+const fs = require('fs');
+const os = require('os');
 
 //导入路由文件
-var article = require('./routes/article')
-var options = require('./routes/options')
-var click = require('./routes/click')
+var article = require('./routes/article');
+var options = require('./routes/options');
+var click = require('./routes/click');
 
-const app = express()
+const app = express();
 
 //设置模板引擎
 app.disable('x-powered-by');
-app.set('views','./views')
-app.set('view engine','ejs')
+app.set('views','./views');
+app.set('view engine','ejs');
 
 //使用静态文件及路由
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static('public'))
-app.use('/article',article)
-app.use('/options',options)
-app.use('/cli',click)
+app.use(express.static('public'));
+app.use('/article',article);
+app.use('/options',options);
+app.use('/cli',click);
 
 //首页
 app.get('/',function(req,res){
     res.sendFile(__dirname+"/"+"index.html")
-})
+});
 
 //404
 app.get(/a/,function(req,res){
-    res.render('3')
-})
+    res.render('404_1');
+});
 
 //404
 app.get('*',function(req,res){
-    res.render('4')
-})
+    res.render('404_2');
+});
 
 //监听运行端口
 app.listen(3000,function(){
-    console.log("Running...")
-})
+    console.log("Running...");
+});
